14d259a29645e42ee0a77679b4816cf6fcd4457e
Gajus Kuizinas <gajus@gajus.com>
2018-12-12 10:54:52 +0000

    feat: add ability to include surrounding lines of log around the matching message

--- src/bin/commands/filter.js
+++ src/bin/commands/filter.js
@@ -2,34 +2,68 @@
 
 import split from 'split2';
 import through from 'through2';
-import jq from 'node-jq';
+import {
+  run as jq
+} from 'node-jq';
 import {
   isRoarrLine
 } from './utilities';
 
 type ArgvType = {|
+  +context: number,
   +excludeOrphans: boolean,
   +jqExpression: string
 |};
 
 type LogFilterConfigurationType = {|
+  +context: number,
   +excludeOrphans: boolean,
   +jqExpression: string
 |};
 
-const filterLog = (run, configuration: LogFilterConfigurationType, line: string, callback: (error?: Error, line?: string) => {}) => {
+let consequentLinePrinted = 0;
+let lastLinePrinterLinesAgo = 0;
+let printNextLines = 0;
+let buffer = [];
+
+const filterLog = (configuration: LogFilterConfigurationType, line: string, callback: (error?: Error, line?: string) => {}) => {
   if (!isRoarrLine(line)) {
     callback(undefined, configuration.excludeOrphans ? '' : line + '\n');
 
     return;
   }
 
-  run(configuration.jqExpression, line, {
+  buffer.push(line);
+
+  buffer = buffer.slice(-1 * configuration.context - 1);
+
+  jq(configuration.jqExpression, line, {
     input: 'string',
     output: 'string'
   })
     .then((data) => {
-      return callback(undefined, data ? data + '\n' : '');
+      let result;
+
+      if (data) {
+        result = buffer.slice(-1 * lastLinePrinterLinesAgo - 1, -1).join('\n') + '\n' + data + '\n';
+
+        consequentLinePrinted++;
+        lastLinePrinterLinesAgo = 0;
+        printNextLines = configuration.context;
+      } else {
+        printNextLines--;
+
+        if (printNextLines >= 0) {
+          result = line + '\n';
+        } else {
+          result = '';
+        }
+
+        consequentLinePrinted = 0;
+        lastLinePrinterLinesAgo++;
+      }
+
+      return callback(undefined, result);
     })
     .catch((error) => {
       return callback(error);
@@ -43,6 +77,11 @@ export const desc = 'Filter Roarr messages using jq.';
 export const builder = (yargs: Object) => {
   return yargs
     .options({
+      context: {
+        default: 2,
+        description: 'Print a number of lines leading and trailing context surrounding each match.',
+        type: 'number'
+      },
       'exclude-orphans': {
         default: false,
         description: 'Excludes messages that cannot be recognized as Roarr log message.',
@@ -58,7 +97,7 @@ export const handler = (argv: ArgvType) => {
     .pipe(through((chunk, encoding, callback) => {
       const line = chunk.toString();
 
-      filterLog(jq.run, argv, line, callback);
+      filterLog(argv, line, callback);
     }))
     .pipe(process.stdout);
 };
