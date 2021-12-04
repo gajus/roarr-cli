669300237d3c8d73c1df6ae1bf6327800e4c295b
Gajus Kuizinas <gajus@gajus.com>
2021-12-04 12:02:22 -0600

    fix: do not keep program up with socket is the only thing alive

--- src/factories/createRemoteStream.ts
+++ src/factories/createRemoteStream.ts
@@ -24,6 +24,7 @@ export const createRemoteStream = (
   let streamConfiguration: StreamConfiguration;
 
   const socket = io(apiUrl, {
+    autoUnref: true,
     query: {
       hostname: os.hostname(),
       name: name || '',
