38cb4ce98710e5a4b97fdd2d148683ffff6d3704
Gajus Kuizinas <gajus@gajus.com>
2023-02-25 11:51:43 -0800

    fix: correct test assertion

--- src/utilities/formatMessage.test.ts
+++ src/utilities/formatMessage.test.ts
@@ -3,16 +3,23 @@ import chalk from 'chalk';
 import { expect, it } from 'vitest';
 
 it('pretty prints Roarr message', () => {
+  const lastMessageTime = 1_538_037_307_418;
+
   expect(
     formatMessage(
       {
         context: { logLevel: 20 },
         message: 'foo',
         sequence: '0',
-        time: 1_538_037_307_418,
+        time: lastMessageTime,
         version: '2.0.0',
       },
-      { chalk, includeDate: true, useColors: false },
+      {
+        chalk,
+        includeDate: true,
+        lastMessageTime: lastMessageTime - 100,
+        useColors: false,
+      },
     ),
-  ).toBe('[2018-09-27T08:35:07.418Z]  1ms debug: foo\n');
+  ).toBe('[2018-09-27T08:35:07.418Z] 100ms debug: foo\n');
 });
