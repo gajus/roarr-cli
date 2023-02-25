459c3e9b7e128987650fcb169cc7fdf9644aafb4
Gajus Kuizinas <gajus@gajus.com>
2023-02-25 07:28:56 -0800

    refactor: abstract format message logic

--- src/factories/createLogFormatter.ts
+++ src/factories/createLogFormatter.ts
@@ -4,14 +4,104 @@ import {
   formatInvalidInputMessage,
 } from '../utilities';
 import prettyjson from 'prettyjson';
-import { type Message } from 'roarr';
+import { type LogLevelName, type Message } from 'roarr';
 import { getLogLevelName } from 'roarr';
 import split from 'split2';
+
+type LogLevelColorMap = {
+  [K in LogLevelName]: (message: string) => string;
+};
+
+const formatMessage = (
+  message: Message,
+  {
+    includeDate,
+    logLevelColorMap,
+    useColors,
+  }: {
+    includeDate: boolean;
+    logLevelColorMap: LogLevelColorMap;
+    useColors: boolean;
+  },
+): string => {
+  let formattedMessage = '';
+
+  if (includeDate) {
+    formattedMessage = '[' + new Date(message.time).toISOString() + ']';
+  } else {
+    formattedMessage =
+      '[' + new Date(message.time).toISOString().slice(11, -1) + ']';
+  }
+
+  if (
+    message.context.logLevel &&
+    typeof message.context.logLevel === 'number'
+  ) {
+    const logLevelName = getLogLevelName(message.context.logLevel);
+
+    const logLevelColorName = logLevelColorMap[logLevelName];
+
+    if (!logLevelColorName) {
+      throw new Error('Unexpected state.');
+    }
+
+    if (message.context.logLevel % 10 === 0) {
+      formattedMessage += ' ' + logLevelColorName(logLevelName);
+    } else {
+      formattedMessage +=
+        ' ' +
+        logLevelColorName(
+          logLevelName + ' (' + String(message.context.logLevel) + ')',
+        );
+    }
+  }
+
+  if (message.context.package) {
+    formattedMessage += ' @' + String(message.context.package);
+  }
+
+  if (message.context.program) {
+    formattedMessage += ' %' + String(message.context.program);
+  }
+
+  if (message.context.namespace) {
+    formattedMessage += ' #' + String(message.context.namespace);
+  }
+
+  formattedMessage += ': ' + message.message + '\n';
+
+  if (message.context) {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const {
+      application: temporary0,
+      hostname: temporary1,
+      instanceId: temporary2,
+      logLevel: temporary3,
+      namespace: temporary4,
+      package: temporary5,
+      program: temporary6,
+      ...rest
+    } = message.context;
+
+    /* eslint-enable @typescript-eslint/no-unused-vars */
+
+    if (Object.keys(rest).length) {
+      formattedMessage +=
+        String(
+          prettyjson.render(rest, {
+            noColor: !useColors,
+          }),
+        ) + '\n\n';
+    }
+  }
+
+  return formattedMessage;
+};
 
 export const createLogFormatter = (
   configuration: LogFormatterConfigurationType,
 ) => {
-  const { chalk, includeDate } = configuration;
+  const { chalk, includeDate, useColors } = configuration;
 
   const logLevelColorMap = {
     debug: chalk.gray,
@@ -22,88 +112,17 @@ export const createLogFormatter = (
     warn: chalk.yellow,
   };
 
-  const formatMessage = (message: Message): string => {
-    let formattedMessage = '';
-
-    if (includeDate) {
-      formattedMessage = '[' + new Date(message.time).toISOString() + ']';
-    } else {
-      formattedMessage =
-        '[' + new Date(message.time).toISOString().slice(11, -1) + ']';
-    }
-
-    if (
-      message.context.logLevel &&
-      typeof message.context.logLevel === 'number'
-    ) {
-      const logLevelName = getLogLevelName(message.context.logLevel);
-
-      const logLevelColorName = logLevelColorMap[logLevelName];
-
-      if (!logLevelColorName) {
-        throw new Error('Unexpected state.');
-      }
-
-      if (message.context.logLevel % 10 === 0) {
-        formattedMessage += ' ' + logLevelColorName(logLevelName);
-      } else {
-        formattedMessage +=
-          ' ' +
-          logLevelColorName(
-            logLevelName + ' (' + String(message.context.logLevel) + ')',
-          );
-      }
-    }
-
-    if (message.context.package) {
-      formattedMessage += ' @' + String(message.context.package);
-    }
-
-    if (message.context.program) {
-      formattedMessage += ' %' + String(message.context.program);
-    }
-
-    if (message.context.namespace) {
-      formattedMessage += ' #' + String(message.context.namespace);
-    }
-
-    formattedMessage += ': ' + message.message + '\n';
-
-    if (message.context) {
-      /* eslint-disable @typescript-eslint/no-unused-vars */
-      const {
-        application: temporary0,
-        hostname: temporary1,
-        instanceId: temporary2,
-        logLevel: temporary3,
-        namespace: temporary4,
-        package: temporary5,
-        program: temporary6,
-        ...rest
-      } = message.context;
-
-      /* eslint-enable @typescript-eslint/no-unused-vars */
-
-      if (Object.keys(rest).length) {
-        formattedMessage +=
-          String(
-            prettyjson.render(rest, {
-              noColor: !configuration.useColors,
-            }),
-          ) + '\n\n';
-      }
-    }
-
-    return formattedMessage;
-  };
-
   return split((line) => {
     if (!findRoarrMessageLocation(line)) {
       return line + '\n';
     }
 
     try {
-      return formatMessage(JSON.parse(line));
+      return formatMessage(JSON.parse(line), {
+        includeDate,
+        logLevelColorMap,
+        useColors,
+      });
     } catch (error) {
       return formatInvalidInputMessage(chalk, error, line);
     }
