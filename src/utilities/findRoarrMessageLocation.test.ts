ef37ee59557a0edcb46a828c6f06d9e180499ada
Gajus Kuizinas <gajus@gajus.com>
2023-02-25 07:21:23 -0800

    chore: use vitest

---
+++ src/utilities/findRoarrMessageLocation.test.ts
@@ -0,0 +1,28 @@
+import { findRoarrMessageLocation } from './findRoarrMessageLocation';
+import { expect, it } from 'vitest';
+
+it('identifies Roarr message at the start of a string', () => {
+  expect(
+    findRoarrMessageLocation(
+      '{"context":{"logLevel":20},"message":"foo","sequence":0,"time":1538037307418,"version":"2.0.0"}',
+    ),
+  ).toStrictEqual({
+    end: 77,
+    start: 0,
+  });
+});
+
+it('identifies prefixed Roarr message', () => {
+  expect(
+    findRoarrMessageLocation(
+      '   {"context":{"logLevel":20},"message":"foo","sequence":0,"time":1538037307418,"version":"2.0.0"}',
+    ),
+  ).toStrictEqual({
+    end: 80,
+    start: 3,
+  });
+});
+
+it('returns null if Roarr message is not found', () => {
+  expect(findRoarrMessageLocation('foo bar')).toBe(null);
+});
